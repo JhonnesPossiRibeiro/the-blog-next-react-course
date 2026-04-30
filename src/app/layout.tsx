cb465ff1edc999027a00fb63a62f4eeff1045bcb
JhonnesPossiRibeiro <jh.possi@gmail.com>
2026-04-30 11:37:34 -0300

    primeiro commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "The blog - Este é um blog com Next.js",
+  description: "Essa sera a descrição dessa página.",
+};
+
+type RootLayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
+  return (
+    <html lang="pt-BR">
+      <body>
+        <header>Header</header>
+       <div className="bg-red-400">{children}</div>
+        <footer>Footer</footer>
+      </body>
+    </html>
+  );
+}
+ 
