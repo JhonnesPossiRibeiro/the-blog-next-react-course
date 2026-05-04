fdb1335f7643ffb46be7c5b913e9c18715417d89
JhonnesPossiRibeiro <jh.possi@gmail.com>
2026-05-04 17:13:17 -0300

    component container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
+import { Container } from '@/components/Container';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
 import { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
-    <div>
+    <Container>
       <header className='text-6xl font-bold text-center py-8'>
         Aqui é a header
       </header>
+
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
       <footer className='text-6xl font-bold text-center py-8'>Footer</footer>
-    </div>
+    </Container>
   );
 }
