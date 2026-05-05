acdbcefd489e276f8f2f50f7cbb2330541d79723
JhonnesPossiRibeiro <jh.possi@gmail.com>
2026-05-05 13:32:48 -0300

    heading h1 e h2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Container } from '@/components/Container';
 import { Header } from '@/components/Header';
+import { PostHeading } from '@/components/PostHeading';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
 import Image from 'next/image';
@@ -22,10 +23,14 @@ export default async function HomePage() {
           />
         </Link>
         <div className='flex flex-col gap-4 sm:justify-center'>
-          <time className='text-slate-600 text-sm/tight' dateTime='2026-05-04'>04/05/2026 17:30</time>
-          <h1 className='text-2xl/tight font-extrabold sm:text-4xl'>
-            <Link href='#'>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</Link>
-          </h1>
+          <time className='text-slate-600 text-sm/tight' dateTime='2026-05-04'>
+            04/05/2026 17:30
+          </time>
+
+          <PostHeading url='#' as='h1'>
+            culpa? Ducimus ipsum sequi dolorum? Magni.
+          </PostHeading>
+
           <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate
             voluptate nobis totam nisi vitae dolores blanditiis praesentium,
